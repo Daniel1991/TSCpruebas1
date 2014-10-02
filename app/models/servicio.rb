fb3fcbba8a08f14b1cd7480539dc71f8b2d4de24
Daniel1991 <danielalexander@hotmail.es>
2014-10-01 20:56:07 -0500

    Se elimino cliente_id en la tabla servicios  en la base de datos, mostraba error al realizar rake db:migrate y se agregaron las validaciones al formulari de servicios

--- app/models/servicio.rb
+++ app/models/servicio.rb
@@ -1,4 +1,25 @@
 class Servicio < ActiveRecord::Base
   belongs_to :cliente
   belongs_to :tipoServicio
+
+      # validar formato de un email
+validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+message: 'Formato no valido' }
+
+# Validar que los atributos sean obligatorios
+validates :nombre, :presence => true 
+validates :identificacion, :presence => true
+validates :email, :presence => true
+validates :direccion, :presence => true
+validates :descripcion, :presence => true
+validates :tipoServicio_id, :presence => true
+validates :telefono, :presence => true
+
+
+# valida la longitud de un campo, tanto la longitud minima como la maxima
+validates :identificacion, length: { minimum: 5, maximum: 13 }
+validates :nombre, length: { minimum: 5, maximum: 50 }
+
+# Valida si un atributo es numerico
+validates :telefono, numericality: true
 end
